072295903c9b109178029ca898c47c0da86196fc
Saul Fileen-Orchover <saul.fileen-orchover@manchester.student.ac.uk>
2024-02-07 13:07:43 +0000

    rename login to sign_up

---
+++ sign_up.js
@@ -0,0 +1,14 @@
+function login() {
+    var fullName = document.getElementById("fullName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // Perform login validation and other actions here
+
+    // Example of printing the values entered
+    console.log("Full Name: " + fullName);
+    console.log("Email ID: " + email);
+    console.log("Password: " + password);
+    console.log("Confirm Password: " + confirmPassword);
+}
