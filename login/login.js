57a6ccddd6b0d2d30f000ca56ab170a6ba250783
j35393lo <louie.obrien@student.manchester.ac.uk>
2024-02-20 13:02:26 +0000

    added temporary salt value 'salt' for testing. Waiting to debug login.js as issue with hash function returning object promise to database

--- login/login.js
+++ login/login.js
@@ -3,8 +3,66 @@ function login() {
     var password = document.getElementById("password").value;
 
     // Perform login validation and other actions here
+    if (password != confirmPassword) {
+        alert("Password does not match confirmation password");
+    } else if (password.length < 8) {
+        alert("Password must be at least 8 characters long")
+    } else {
+        // generate user ID, create new user in database, give user confirmation of created account, redirect to index
+        let salt = createSalt();
+
+        //let passwordHash = hashPassword(password, salt);
+
+        storeData(username, password, email, salt);
+
+        //hashPassword(password, salt).then((hashedPassword) => {
+        //    createNewUser(username, hashedPassword, email, salt);
+        //});
+    }
 
     // Example of printing the values entered
     console.log("User Name: " + username);
     console.log("Password: " + password);
+    console.log("Confirm Password: " + confirmPassword);
+}
+
+function createSalt() {
+    return Math.random().toString(36).substring(2, 7);
+}
+
+async function hashPassword(password, salt) {
+    var msgBuffer = new TextEncoder().encode(password + salt);
+    var hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    var hashArray = Array.from(new Uint8Array(hashBuffer));
+    var hashHex = hashArray.map(byte => ('00' + byte.toString(16)).slice(-2)).join('');
+    return hashHex;
+}
+
+async function createNewUser(username, passwordHash, email, salt) {
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", "createNewUser.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const response_text = xhr.responseText;
+                //document.getElementById("responseContainer").innerHTML = response_text;
+            }
+            else {
+                console.error('Error occurred: ' + xhr.status);
+            }
+        }
+    };
+    xhr.send("username=" + username + "&passwordHash=" + passwordHash + "&email=" + email + "&salt=" + salt);
+}
+
+async function storeData(username, password, email, salt) {
+    try {
+        var hashedPassword = await hashPassword(passowrd, salt);
+        await createNewUser(username, hashedPassowrd, email, salt);
+        return "user created";
+    } catch (error) {
+        retunr "error creating user";
+    }
 }
