1d07f89eb6d4e314e51377222a86beecddcd378e
j22352sa <samuel.auterson@student.manchester.ac.uk>
2024-02-21 20:55:54 +0000

    Updated home.js to call getPlaces.php to get place data from database CURRENTLY NOT WORKING - something is wrong with the Location column, - maybe the fact it's stored as GEOMETRY is fucking things up

--- home/getPlaces.php
+++ home/getPlaces.php
@@ -1,18 +1,59 @@
 <?php
 
-function getPlaces() {
+
+header('Content-Type: application/json');
+
+$aResult = array();
+
+if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+//echo $_POST['functionname'];
+//if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+
+if( !isset($aResult['error']) ) {
+
+    switch($_POST['functionname']) { //using switch case in case i ever want to call different functions with this code
+        case 'getPlaces':
+            $aResult['result'] = getPlaces();
+            //echo $aResult; //just check if it's working; we really want to send back a json
+            break;
+        default:
+            $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+            echo '2b';
+            break;
+    }
+
+}
+
+echo json_encode($aResult);
+
+function getPlaces() { //this function works! don't mess around too much pls :) - had to change a column name in db to make it work as 'Location' is a php keyword i think
     $database_host = "dbhost.cs.man.ac.uk";
     $database_user = "j22352sa";
-    $database_pass = "<the password you set>";
+    $database_pass = "cooldatabasepassword";
     $database_name = "2023_comp10120_cm7";
     $conn = new mysqli($database_host,$database_user, $database_pass, $database_name);
     if($conn->connect_error){
-        die("Connection failed ".$conn->connect_error);
+        echo 'Connection to Database Error';
     }
 
-    $sqlGetPlaces = "SELECT Name,Location FROM Place";
-    $result = $conn->query($sqlGetPlaces);
-    return $result;
+    $sqlGetPlaces = "SELECT PlaceName FROM Place"; //why is querying Location returning an error?
+
+
+    $result = mysqli_query($conn, $sqlGetPlaces); //returns mysqli_result object not array ! (im a fool)
+    $return = array();
+    if (mysqli_num_rows($result) > 0) {
+        foreach($result as $row) {
+            $return[] = $row;
+            //echo $row['LocationLatLng']; //checking LocationLatLng actually returned; 
+        }
+    } else {
+        $return = 'Query Failed'; //this is not a good error message but
+    }
+    $conn->close();
+    return $return;
 }
+
+
 
 ?>
--- home/home.js
+++ home/home.js
@@ -1,15 +1,45 @@
-var placeData = placeDataSent;
 
 //creates map
-function initMap() {
-    const myLatLng = { lat: 53.480759, lng: -2.242631 }
-    //makes map object
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: myLatlng,
-      });
+async function initMap() {
+  var placeData;
+  await jQuery.ajax({
+    type: "POST",
+    url: 'getPlaces.php',
+    dataType: 'json',
+    data: {functionname: 'getPlaces'},
+
+    success: function (obj, textstatus) {
+                  if( !('error' in obj) ) {
+                      placeData = obj.result;
+                  }
+                  else {
+                      console.log(obj.error);
+                  }
+            }
+  }).done(function(data) {
+    console.log(data);})
+    .fail(function( xhr, status, errorThrown ) {
+      alert( "Sorry, there was a problem!" );
+      console.log( "Error: " + errorThrown );
+      console.log( "Status: " + status );
+      console.dir( xhr );
+    });
+
+  console.log(placeData);
+  console.log("heyyyy");
+  document.getElementById("map").setAttribute("innerHTML",placeData);
+
+
+  //const myLatLng = { lat: 53.480759, lng: -2.242631 }
+  //makes map object
+  /*const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatlng,
+    });
 
     //todo: call php to get all the data from db,
     //make markers and add to map, 
-    //return map
+    //return map*/
 }
+
+initMap()
