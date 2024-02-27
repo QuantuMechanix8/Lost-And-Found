9471b8407aa0e479dcd236e4c32e752ede704be7
j22352sa <samuel.auterson@student.manchester.ac.uk>
2024-02-27 09:32:54 +0000

    added markerclusterer (not working), progress on making all pins appear on the map

--- home/home.js
+++ home/home.js
@@ -1,6 +1,6 @@
 
 //creates map
-/*async function getPlaceData() {
+async function getPlaceData() {
   var placeData;
   await jQuery.ajax({
     type: "POST",
@@ -17,8 +17,7 @@
                   }
             }
   }).done(function(data) {
-    console.log(data);
-    var dataOfPlaces = data.result;})
+    var placeData = data;})
     .fail(function( xhr, status, errorThrown ) {
       alert("Sorry, there was a problem!"); //annoying but useful
       console.log("Error: " + errorThrown);
@@ -26,45 +25,65 @@
       console.dir(xhr);
     });
 
-  console.log(placeData)
-
   return await placeData;
 
   
   
 
 
-}*/
+}
 
 let map;
 
 async function initMap() {
 
-  //var placeData = getPlaceData()
+  var placeData = await getPlaceData();
+ 
+  // request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
 
-  const { Map } = await google.maps.importLibrary("maps");
   const myLatLng = { lat: -25.363, lng: 131.044 };
   map = new Map(document.getElementById("map"), {
     zoom: 4,
     center: myLatLng,
   });
 
- /*let Marker = new google.maps.Marker({
+  /*let Marker = new google.maps.Marker({
     position: myLatLng,
   });
 
-  Marker.setMap(map)*/
+  Marker.setMap(map);*/
 
-  /*for (let place of placeData) {
-    position = {place.latitude,place.longitude};
 
-    let Marker = new google.maps.Marker({
-      position: myLatlng
-    })
-    Marker.setMap(map)
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+
+
+  const markers = placeData.map((element,i) => { //go through every element of placedata, make a marker with attached infowindow out of it
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+    positon : { lat: element.latitude, lng : element.longitude},
+    });
+
+
+    marker.addListener("click", () => { //add an infowindow to each marker just for fun
+      infoWindow.setContent(element.PlaceName);
+      infoWindow.open(map, marker);
+    });
+
+    return marker;
+
+  });
   
-  }*/
-}
+  new MarkerClusterer({ markers, map }); //make a marker clusterer so we don't lag too much (i hope)
+
+
+  }
+
 
     //todo: call php to get all the data from db,
     //make markers and add to map, 
