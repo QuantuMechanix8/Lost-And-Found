a5ec2627b6e400becbf67aa6d631875b8038e921
Saul Fileen-Orchover <saul.fileen-orchover@manchester.student.ac.uk>
2024-03-05 03:31:36 +0000

    added website description to frontpage

--- front_page/views/landing_page.js
+++ front_page/views/landing_page.js
@@ -1,9 +1,27 @@
+const openMenuIcon = 'square-caret-down';
+const closedMenuIcon = 'bars';
+
 function toggleMenu() {
-  console.log("toggleMenu");
+  //console.log("toggleMenu");
   var menu = document.getElementById('menu');
+  var button = document.getElementById('menu-button');
   if (menu.style.display === 'flex') {
     menu.style.display = 'none';
+    button.dataset.prefix = 'fas';
+    button.dataset.icon = closedMenuIcon;
   } else {
     menu.style.display = 'flex';
+    button.dataset.prefix = 'far';
+    button.dataset.icon = openMenuIcon;
+  }
+}
+
+function toggleDescription() {
+  //console.log("toggleDescription");
+  var description = document.getElementById('description');
+  if (description.style.display === 'flex') {
+    description.style.display = 'none';
+  } else {
+    description.style.display = 'flex';
   }
 }
